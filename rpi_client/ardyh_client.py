1797345ad0c2d05c1cf29045336fb260fd29a557
Wil Black <wilblack21@gmail.com>
2014-05-04 22:55:12 +0000

    Got client on rpi1 to send messages.

--- rpi_client/ardyh_client.py
+++ rpi_client/ardyh_client.py
@@ -22,6 +22,14 @@ from bot_roles.core import Core
 from utils import get_mac_address
 
 
+
+
+if "jjbot" in settings["bot_packages"]:
+    from BrickPi import *   #import BrickPi.py file to use BrickPi operations
+    import threading
+
+
+
 class ArdyhClient(TornadoWebSocketClient):
     """
     Web Socket client to connect to ardyh on start up.
@@ -38,7 +46,7 @@ class ArdyhClient(TornadoWebSocketClient):
 
     """
 
-    def __init__(self, protocols, uri='ws://173.255.213.55:9093/ws'):
+    def __init__(self, protocols, uri='ws://173.255.213.55:9093/ws?'):
         rs = super(ArdyhClient, self).__init__(uri, protocols)
         
 
@@ -47,6 +55,8 @@ class ArdyhClient(TornadoWebSocketClient):
         self.CTENOPHORE = CTENOPHORE
         
 
+        self.channel = "io.ardyh.{}".format(settings['bot_name'])
+
         # set the name to MAC address if not found.
         self.bot_name = settings['bot_name']
         self.bot_roles = settings['bot_roles']
@@ -54,14 +64,23 @@ class ArdyhClient(TornadoWebSocketClient):
         self.core = Core()
         self.router = Router()
 
+
     def opened(self):
         print "Connection to ardh is open"
         message = {'bot_name':self.bot_name, 
                    'bot_roles':self.bot_roles,
                    'mac':get_mac_address(),
-                   'handshake':True}
+                   'handshake':True,
+                   'subscribe':settings['subscriptions']
+                   }
+
 
         self.send(message)
+
+        if "jjbot" in settings["bot_packages"]:
+
+            sensors = tornado.ioloop.PeriodicCallback(self.loopCallback, 500)
+            sensors.start()
 
 
     def received_message(self, message):
@@ -69,6 +88,25 @@ class ArdyhClient(TornadoWebSocketClient):
 
 
     def send(self, message):
+        """
+        Message should be of the form {MESSAGE_OBJ}
+        
+
+        - message
+        -- bot_name
+        -- from
+        -- message
+        -- command
+        -- channel 
+        -- ardyh_timestamp - May not be present
+
+        """
+
+        channel = settings['bot_name']
+        message.update({
+            "bot_name":self.bot_name,
+            "channel":self.channel
+        })
         message = json.dumps(message)
         if VERBOSE: print "[ArdyhClient.send] Send message:\n\n%s" %(message) 
         try:
@@ -79,7 +117,9 @@ class ArdyhClient(TornadoWebSocketClient):
 
     def closed(self, code, reason=None):
         print "Closed down", code, reason
-        ioloop.IOLoop.instance().stop()
+        
+        if "jjbot" in settings["bot_packages"]:
+            ioloop.IOLoop.instance().stop()
 
 
     def log(self, message):
@@ -90,17 +130,64 @@ class ArdyhClient(TornadoWebSocketClient):
 
 
     def loopCallback(self):
-        if self.JJBOT:
-            sensor_values = self.bot.get_sensors_values()
-            out = {"sensor_values":sensor_values}
-            self.send(json.dumps(out))
+        if "jjbot" in settings["bot_packages"]:
+            sensor_values = self.get_sensors_values() # This is where to sensor values get sent to ardyh
+            out = {"message": {"sensor_values":sensor_values} }
+            self.send(out)
+
+
+    def get_sensors_values(self):
+        out = [
+            ['PORT_1', BrickPi.Sensor[PORT_1]],
+            ['PORT_2', BrickPi.Sensor[PORT_2]],
+            ['PORT_3', BrickPi.Sensor[PORT_3]],
+            ['PORT_4', BrickPi.Sensor[PORT_4]],
+          ]
+        return out
+
+
+class myThread (threading.Thread):
+    """
+    I think this just tells the BrickPi to update the values, their is no networking involved.
+
+    """
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        print "Starting thread %s" %(self.threadID)
+        while sensor_thread_running:
+            result = BrickPiUpdateValues()       # Ask BrickPi to update values for sensors/motors
+            time.sleep(.2)              # sleep for 200 ms
+
+
+
 
 
 if __name__ == "__main__":
 
+    if "jjbot" in settings["bot_packages"]:
+        BrickPiSetup()  # setup the serial port for communication
+        BrickPi.MotorEnable[PORT_A] = 1 #Enable the Motor A
+        BrickPi.MotorEnable[PORT_B] = 1 #Enable the Motor A
+        BrickPi.MotorEnable[PORT_C] = 1 #Enable the Motor D
+        BrickPi.MotorEnable[PORT_D] = 1 #Enable the Motor D
+
+        BrickPi.SensorType[PORT_1] = TYPE_SENSOR_ULTRASONIC_CONT   #Set the type of sensor at PORT_1
+        BrickPiSetupSensors()   #Send the properties of sensors to BrickPi
+
+        sensor_thread_running = True
+        thread1 = myThread(1, "Thread-1", 1)
+        thread1.setDaemon(True)
+        thread1.start()
+
     # Start streaming data to ardyh.
-    ardyh = ArdyhClient(protocols=['http-only', 'chat'])
+    ardyh = ArdyhClient(uri=URI, protocols=['http-only', 'chat'])
     ardyh.connect()
     #starts the websockets connection
     tornado.ioloop.IOLoop.instance().start()
     print "Could not open web socket connect to ardyh"
+
+
