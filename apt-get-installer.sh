12ef4e698ad0ae6ce171d28c80eb69e1b85ade28
Wil Black <wilblack21@gmail.com>
2014-05-21 22:59:47 -0700

    Update apt-get-installer.sh

--- apt-get-installer.sh
+++ apt-get-installer.sh
@@ -8,3 +8,18 @@ sudo apt-get install ipython -y
 sudo apt-get install python-pip -y
 sudo apt-get install bluez -y
 sudo apt-get install python-bluetooth -y
+
+cd /home/pi
+mkdir projects
+cd projects 
+git clone https://github.com/wilblack/lilybot.git
+
+# Install this if using the Brick Pi
+git clone https://github.com/DexterInd/BrickPi_Python.git
+
+# Install this is using LED lights
+git clone https://github.com/adammhaile/RPi-LPD8806
+
+# Install GroverPi
+git clone https://github.com/DexterInd/GrovePi
+
