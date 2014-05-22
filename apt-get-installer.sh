2e77442d6dce183c19f8ebca4793554e09856054
Wil Black <wilblack21@gmail.com>
2014-05-21 22:43:33 -0700

    Create apt-get-installer.sh

---
+++ apt-get-installer.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install git-core -y
+sudo apt-get install screen -y
+sudo apt-get install ipython -y
+sudo apt-get install python-pip -y
+sudo apt-get install bluez -y
+sudo apt-get install python-bluetooth -y
