71423e1b88cad745a4e8505a7047db6375b2d4e2
Wil Black <wilblack21@gmail.com>
2014-05-22 00:50:45 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,6 +5,7 @@ LILYBOT_PATH=/home/pi/projects/lilybot/
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+sudo apt-get install python-dev -y
 sudo apt-get install git-core -y
 sudo apt-get install screen -y
 sudo apt-get install ipython -y
