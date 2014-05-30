d159a11d84a338f493bd05113808be074b8253b9
Wil Black <wilblack21@gmail.com>
2014-05-30 05:40:04 +0000

    Got camera working from the client side.

--- rpi_client/start_server_camera.sh
+++ rpi_client/start_server_camera.sh
@@ -5,6 +5,9 @@
 
 mkdir /tmp/stream
 
+# Not sure what this does but it needs to be here. 
+raspistill --nopreview -w 640 -h 480 -q 5 -o /tmp/stream/pic.jpg -tl 100 -t 9999999 -th 0:0:0 &
+
 # Start the server
 LD_LIBRARY_PATH=/usr/local/lib /usr/local/bin/mjpg_streamer -i "input_file.so -f /tmp/stream -n pic.jpg" -o "output_http.so -w /home/pi/Projects/lilybot/rpi_client/camera_stream/www -p 8081"
 
