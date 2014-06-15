e99c41a906ca3f98e8c683e058269d24d3f6f77b
Wil Black <wilblack21@gmail.com>
2014-06-15 19:49:56 +0000

    Added mahgic mushroom local setting.

---
+++ rpi_client/bot_roles/local_settings_magic_mushroom.py
@@ -0,0 +1,12 @@
+"""
+Router.py uses bot_packages in this file to setup command and sensor value routing to the correct bot_role.
+"""
+
+settings= {
+    "bot_name":"rp2.solalla.ardyh",
+    "bot_roles":"bot",
+    "bot_packages":["magic_mushroom"],
+    "subscriptions":['rp1.solalla.ardyh', 'rp3.solalla.ardyh'],
+    
+}
+NLEDS = 32
