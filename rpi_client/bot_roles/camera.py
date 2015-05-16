008f4e386cfd8b8c7c9f721a3d6977713979a91c
Wil Black <wilblack21@gmail.com>
2015-05-16 06:31:05 +0000

    Added in the capture_image command

--- rpi_client/bot_roles/camera.py
+++ rpi_client/bot_roles/camera.py
@@ -3,12 +3,13 @@ import time
 import picamera
 from datetime import datetime as dt
 
-out_dir = "/home/pi/growbot/"
+out_dir = "/home/pi/projects/lilybot/images/growbot"
 if not os.path.isdir(out_dir):
     os.mkdir(out_dir)
 
 with picamera.PiCamera() as camera:
     camera.resolution = (1280, 720)
+    camera.rotation = 180
     camera.start_preview()
     # camera.exposure_compensation = 2
     # camera.exposure_mode = 'spotlight'
@@ -16,12 +17,24 @@ with picamera.PiCamera() as camera:
     # camera.image_effect = 'gpen'
     # Give the camera some time to adjust to conditions
     time.sleep(2)
-    fname = "image_%s.jpg" %(dt.strftime(dt.now(), "%Y-%m-%dT%H:%M:%SZ"))
+    fname = "image_%s.jpg" %(dt.strftime(dt.now(), "%Y-%m-%dT%H-%M-%SZ"))
     print "Creating image %s in %s" %(fname, out_dir)
     camera.capture(os.path.join(out_dir, fname))
     camera.stop_preview()
 
+    os.system("su pi -c 'rsync -avz /home/pi/projects/lilybot/images/growbot/ wilblack@ardyh.solalla.com:/home/wilblack/projects/lilybot/web_client/growbot'")
 
 """
-*/1 * * * * python /home/pi/projects/lilybot/rpi_client/bot_roles/camera.py > /home/pi/cron.log 2>&1
+12 */2 * * * python /home/pi/projects/lilybot/rpi_client/bot_roles/camera.py > /home/pi/cron.log 2>&1
+
+rsync -avz /home/pi/projects/lilybot/images/growbot/ wilblack@192.168.0.106:/Users/wilblack/Desktop/growbot/
+rsync -avz /home/pi/projects/lilybot/images/growbot/ wilblack@ardyh.solalla.com:/home/wilblack/projects/lilybot/web_client/growbot
+
+
+Possible way to send an image
+
+with open("some_image.png", "rb") as imageFile:
+    self.str = base64.b64encode(imageFile.read())
+
 """
+
