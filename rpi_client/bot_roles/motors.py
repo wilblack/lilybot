94bce4a732eec8bbc7f02e7014325331670af23e
Wil Black <wilblack21@gmail.com>
2016-05-08 21:44:37 -0700

    Update motors.py

--- rpi_client/bot_roles/motors.py
+++ rpi_client/bot_roles/motors.py
@@ -9,30 +9,40 @@ GPIO.cleanup()
 # Physical pins 11,15,16,18
 # GPIO17,GPIO22,GPIO23,GPIO24
 
-StepPinForward=13
-StepPinBackward=15
-sleeptime=1
+# Pins for Motor A (front left)
+aBackward=18 # In 1
+aForward=20 # In 2
+
+# Pins for Motor B (front right)
+bBackward=13 # In 3
+bForward=15 # In 4
+
+
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(StepPinForward, GPIO.OUT)
 GPIO.setup(StepPinBackward, GPIO.OUT)
 
 def forward(x):
-    GPIO.output(StepPinForward, GPIO.HIGH)
+    GPIO.output(aForward, GPIO.HIGH)
+    GPIO.output(bForward, GPIO.HIGH)
     print "forwarding running  motor "
     time.sleep(x)
-    GPIO.output(StepPinForward, GPIO.LOW)
+    GPIO.output(aForward, GPIO.LOW)
+    GPIO.output(aForward, GPIO.LOW)
 
 def reverse(x):
-    GPIO.output(StepPinBackward, GPIO.HIGH)
+    GPIO.output(aBackward, GPIO.HIGH)
+    GPIO.output(bBackward, GPIO.HIGH)
     print "backwarding running motor"
     time.sleep(x)
-    GPIO.output(StepPinBackward, GPIO.LOW)
+    GPIO.output(aBackward, GPIO.LOW)
+    GPIO.output(bBackward, GPIO.LOW)
 
 print "forward motor "
-forward(5)
+forward(3)
 print "reverse motor"
-reverse(5)
+reverse(3)
 
 print "Stopping motor"
 GPIO.cleanup()
